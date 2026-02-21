2fbd7186880e0b90029490e6c4f6a787067b544d
jorge <jorgebotero190@gmail.com>
2026-02-20 22:19:00 -0500

    Crea clases EstruturaDeRepeticion

---
+++ src/main/java/com/example/EstructurasDeRepeticion.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class EstructurasDeRepeticion {
+
+public static void ejemplosWhile() {
+    int i = 0;
+
+    while (i < 5) {
+        System.out.println("El valor de i es: " + i);
+
+
+}
+}}
