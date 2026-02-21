7d74e11878e0f386f1633b6e4deaa559ff955cf4
jorge <jorgebotero190@gmail.com>
2026-02-20 22:32:51 -0500

    Crea componentes que me dan ejemplos de ciclos for

--- src/main/java/com/example/EstructurasDeRepeticion.java
+++ src/main/java/com/example/EstructurasDeRepeticion.java
@@ -7,7 +7,63 @@ public static void ejemplosWhile() {
 
     while (i < 5) {
         System.out.println("El valor de i es: " + i);
+}
+}
+
+public static void ejemplosDoWhile() {
+    int i = 1;
+    do {
+        System.out.println("El valor de i es: " + i);
+
+}
+    while (i < 5);
+}
+
+public static void ejemplosForClásico(){
+    for (int i = 0; i < 5; i++) {
+        System.out.println("El valor de i es: " + i);
+}
+}
+public static void ejemplosForAnidado(){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+
+            System.out.println("El valor de i es: " + i + " y el valor de j es: " + j);
+        }
+    }
+}
+public static void ejemplosBreakContinue(){
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) {
+            break;
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) {
+            continue;
+        }
+        System.out.println("El valor de i es: " + i);
+    }
+
+}
+public static void ejemplosEtiquetas(){
+    externo:
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == 1 && j == 1) {
+                break externo;
+            }
+            System.out.println( i + ", " + j);
+        }
+    }
+}
 
 
 }
-}}
+
+
+
+
+
+
+
