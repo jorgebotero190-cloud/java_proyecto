b5885a23335b47735ab8f91c14d958bcc8d867a0
jorge <your.email@example.com>
2026-03-10 19:34:58 -0500

    Optimización de ejemplos de bucles

--- src/main/java/com/example/EstructurasDeRepeticion.java
+++ src/main/java/com/example/EstructurasDeRepeticion.java
@@ -2,69 +2,77 @@ package com.example;
 
 public class EstructurasDeRepeticion {
 
-public static void ejemplosWhile() {
-    int i = 0;
-
-    while (i < 5) {
-        System.out.println("El valor de i es: " + i);
-        i++;
-}
-}
-
-public static void ejemplosDoWhile() {
-    int i = 1;
-    do {
-        System.out.println("El valor de i es: " + i);
-        i++;
-}
-    while (i < 5);
-}
-
-public static void ejemplosForClásico(){
-    for (int i = 0; i < 5; i++) {
-        System.out.println("El valor de i es: " + i);
-}
-}
-public static void ejemplosForAnidado(){
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-
-            System.out.println("El valor de i es: " + i + " y el valor de j es: " + j);
+    public static void ejemplosWhile() {
+        System.out.println("=== WHILE ===");
+        int i = 0;
+        while (i < 5) {
+            System.out.println("El valor de i es: " + i);
+            i++;
         }
-    }
-}
-public static void ejemplosBreakContinue(){
-    for (int i = 0; i < 10; i++) {
-        if (i == 5) {
-            break;
-        }
-    }
-    for (int i = 0; i < 10; i++) {
-        if (i == 5) {
-            continue;
-        }
-        System.out.println("El valor de i es: " + i);
+        System.out.println();
     }
 
-}
-public static void ejemplosEtiquetas(){
-    externo:
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (i == 1 && j == 1) {
-                break externo;
+    public static void ejemplosDoWhile() {
+        System.out.println("=== DO-WHILE ===");
+        int i = 1;
+        do {
+            System.out.println("El valor de i es: " + i);
+            i++;
+        } while (i < 5);
+        System.out.println();
+    }
+
+    public static void ejemplosForClasico() {
+        System.out.println("=== FOR CLÁSICO ===");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("El valor de i es: " + i);
+        }
+        System.out.println();
+    }
+
+    public static void ejemplosForAnidado() {
+        System.out.println("=== FOR ANIDADO ===");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("i = " + i + ", j = " + j);
             }
-            System.out.println( i + ", " + j);
         }
+        System.out.println();
+    }
+
+    public static void ejemplosBreakContinue() {
+        System.out.println("=== BREAK ===");
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.println("Se encontró i = 5, se rompe el bucle.");
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("\n=== CONTINUE ===");
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                System.out.println("Se encontró i = 5, se salta esta iteración.");
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+        System.out.println();
+    }
+
+    public static void ejemplosEtiquetas() {
+        System.out.println("=== ETIQUETAS ===");
+        externo:
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    System.out.println("Se encontró i=1 y j=1, se rompe el bucle externo.");
+                    break externo;
+                }
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+        System.out.println();
     }
 }
-
-
-}
-
-
-
-
-
-
-
